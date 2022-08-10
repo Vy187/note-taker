3fed63d972599f7bdfb1d24b7a88c7a10482571f
Vy Nguyen <vy1872@gmail.com>
2022-08-10 16:16:59 -0700

    routed the server to the route/index.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require(`express`);
 const path = require(`path`);
+const api = require(`./routes/index`)
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(`/api`, api)
 
 app.use(express.static(`public`));
 
